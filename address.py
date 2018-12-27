eff9d7c9fb51839897dc05c3d07d38dfb567a56b
sewon-swjh0121 <swjh0121@gmail.com>
2018-12-28 00:58:44 +0900

    Set Basic Data type

---
+++ address.py
@@ -0,0 +1,7 @@
+#Set Data
+
+class Address:
+
+    def __init__(self):
+        self.name = ''
+        self.number =''
---
+++ addressmanager.py
@@ -0,0 +1,5 @@
+from address import Address
+
+class AddressManager():
+
+    
