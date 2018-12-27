fb41a3aecf3fb9565fd43e169fe756d6ef66c2a6
sewon-swjh0121 <swjh0121@gmail.com>
2018-12-28 01:41:19 +0900

    Modify main.py

--- main.py
+++ main.py
@@ -2,15 +2,45 @@ from address import Address
 from addressmanager import AddressManager
 import bottle
 
+global manager
+manager = AddressManager()
 
 
+@bottle.route('/show')
+def show_page():
+    result = manager.show_address()
+    return result
 
 
+@bottle.route('/modify/<userInput>')
+def modify_page(userInput):
+    temp = userInput.split('|')
 
-def main():
+    initialData = Address()
+    initialData.name = temp[0]
+    initialData.number = temp[1]
+    state = manager.modify_address(temp[0],initialData)
+    if(state is None):
+        str = "nothing modified."
+        return str
+    else:
+        str = "complete."
+        return str
 
 
+@bottle.route('/add/<userInput>')
+def add_page(userInput):
+    temp = userInput.split('|')
+
+    #set data
+    initialData = Address()
+    initialData.name = temp[0]
+    initialData.number = temp[1]
+    manager.add_address(initialData)
+
+    #console log
+    manager.show_address()
 
 
-if __name__ == "__main__":
-    main()
+bottle.debug(True)
+bottle.run(host='localhost',port=8080)
